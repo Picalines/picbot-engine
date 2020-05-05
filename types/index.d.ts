e58bffe29db050b8c61be6b54b8c54973448b280
Petr Kostenko <picalines0@gmail.com>
2020-05-05 18:17:43 +0300

    clean up

--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { ArgumentReader, ArgumentParser, Argument, KeywordArgument, RemainingTextArgument, CharReader, NumberArgument, RegexReader, RegexArgument, MemberArgument, } from "./argument";
-export { CommandInfo, CommandBranch, CommandMainBranch, Command, } from "./commands";
-export { Bot } from "./bot";
