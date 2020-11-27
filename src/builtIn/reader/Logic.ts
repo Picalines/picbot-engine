ffb136f8d76db7f1524ac3007567330ac483acac
Petr Kostenko <picalines0@gmail.com>
2020-11-27 16:38:55 +0300

    optionalReader overloads to fix intellisense

--- src/builtIn/reader/Logic.ts
+++ src/builtIn/reader/Logic.ts
@@ -1,11 +1,15 @@
 import { ArgumentReader } from "../../command/argument/Reader";
+import { Primitive } from "../../utils";
 import { spaceReader } from "./String";
 
 /**
- * @returns функцию, которая либо читает аргумент, либо возвращает _default, если аргумент не найден
+ * @returns функцию, которая либо читает аргумент, либо возвращает стандартное значение, если аргумент не найден
  * @param reader функция, читающая аргумент
  */
-export function optionalReader<T, D extends T | null>(reader: ArgumentReader<T>, defaultValue: D = null!): ArgumentReader<T | D> {
+export function optionalReader<T>(reader: ArgumentReader<T>, defaultValue?: null): ArgumentReader<T | null>
+export function optionalReader<T>(reader: ArgumentReader<T>, defaultValue: T): ArgumentReader<T>
+export function optionalReader<T, D extends Primitive>(reader: ArgumentReader<T>, defaultValue: D): ArgumentReader<T | D>
+export function optionalReader<T, D>(reader: ArgumentReader<T>, defaultValue: D = null!): ArgumentReader<T | D> {
     return (userInput, context) => {
         const result = reader(userInput, context);
         if (result.isError && !userInput.length) {
