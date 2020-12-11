e7ddabc48972371bb1ab140b66cb956afdba67dd
Petr Kostenko <picalines0@gmail.com>
2020-12-12 00:22:42 +0300

    export case fix

--- src/utils/cache/index.ts
+++ src/utils/cache/index.ts
@@ -1,3 +1,3 @@
-export * from './Cache';
-export * from './Factory';
-export * from './Grouped';
+export * from "./Cache";
+export * from "./Factory";
+export * from "./Grouped";
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,5 @@ export * from "./UsefulFunctions";
 export * from "./Literal";
 export * from "./Logic";
 export * from "./Message";
-export * from "./Cache";
+export * from "./cache";
 export * from "./Constants";
