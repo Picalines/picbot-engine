b566fd6e187c84b26a259e796148131b1e7ad4f4
Petr Kostenko <picalines0@gmail.com>
2021-01-03 20:47:30 +0300

    lazy loaded terms

--- src/translator/Translator.ts
+++ src/translator/Translator.ts
@@ -12,39 +12,15 @@ export class Translator {
     readonly #translations = new WeakMap<TermCollection<any>, Map<string, TranslationCollection<any>>>();
 
     constructor(readonly bot: Bot) {
-        const addTerms = (terms: TermCollection<any>) => {
-            if (!this.#translations.has(terms)) {
-                this.#translations.set(terms, new Map());
-            }
-        }
-
-        this.bot.loadingSequence.stage('require terms', () => {
-            addTerms(argumentReaderTerms);
-
-            if (this.bot.options.useBuiltInHelpCommand) {
-                addTerms(helpEmbedTerms);
-                this.#translations.get(helpEmbedTerms)!.set(helpEmbedTranslationsRU.locale, helpEmbedTranslationsRU);
-            }
-
-            requireFolder(TermCollection, this.bot.options.loadingPaths.terms).forEach(([path, terms]) => {
-                addTerms(terms);
-                this.bot.logger.log(path);
-            });
-        });
-
-        this.bot.commands.events.on('added', command => addTerms(command.terms));
-
         this.bot.loadingSequence.after('require commands', 'require translations', () => {
-
-            this.#translations.get(helpCommand.terms)?.set('ru', helpInfoTranslationRU);
-            this.#translations.get(argumentReaderTerms)?.set('ru', argumentReaderTermTranslationRU);
+            this.termsMap(helpCommand.terms).set('ru', helpInfoTranslationRU);
+            this.termsMap(helpEmbedTerms).set('ru', helpEmbedTranslationsRU);
+            this.termsMap(argumentReaderTerms).set('ru', argumentReaderTermTranslationRU);
 
             requireFolder(TranslationCollection, this.bot.options.loadingPaths.translations).forEach(([path, translations]) => {
                 this.bot.logger.log(path);
 
-                assert(this.#translations.has(translations.terms), `${TranslationCollection.name} uses not loaded ${TermCollection.name} object`);
-
-                const map = this.#translations.get(translations.terms)!;
+                const map = this.termsMap(translations.terms);
                 const { locale } = translations;
 
                 if (map.has(locale)) {
@@ -58,14 +34,25 @@ export class Translator {
     }
 
     /**
+     * @returns карта переводов для нужной коллекции терминов
+     */
+    private termsMap(terms: TermCollection<any>): Map<string, TranslationCollection<any>> {
+        let map = this.#translations.get(terms);
+        if (!map) {
+            map = new Map();
+            this.#translations.set(terms, map);
+        }
+        return map;
+    }
+
+    /**
      * @returns коллекцию переводов терминов на нужную локаль
      * @param terms коллекция терминов
      * @param toLocale локаль, на которую нужно перевести термины
      */
     translations<Contexts extends TermContexts>(terms: TermCollection<Contexts>, toLocale: string): TranslationCollection<Contexts> {
         assert(toLocale, 'invalid locale');
-        assert(this.#translations.has(terms), `unkown ${TermCollection.name} object`);
-        return (this.#translations.get(terms)!.get(toLocale) ?? terms.defaultTranslations) as TranslationCollection<Contexts>;
+        return (this.termsMap(terms).get(toLocale) ?? terms.defaultTranslations) as TranslationCollection<Contexts>;
     }
 
     /**
