a4d99aa1de00e0e038520bb5055d77537832a44e
Petr Kostenko <picalines0@gmail.com>
2021-03-02 20:43:23 +0300

    Command.ts obsolete docs removed

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -5,90 +5,34 @@ import { Bot } from "../bot/index.js";
 import { TermCollection } from "../translator/index.js";
 import { ArgumentSequence } from "./argument/index.js";
 
-/**
- * Информация о команде
- */
-export interface CommandInfo<Args extends unknown[]> {
-    /**
-     * Имя команды. Оно не должно содержать пробелов. Все буквы должны быть строчными.
-     */
-    readonly name: string;
-
-    /**
-     * Алиасы ("другие имена команды"). На алиасы распространяются те же
-     * правила, что и на [[CommandInfo.name]]
-     */
-    readonly aliases?: Readonly<NonEmpty<string[]>>;
-
-    /**
-     * Аргументы команды
-     */
-    readonly arguments?: ArgumentSequence<Args>;
-
-    /**
-     * Права участника сервера
-     */
-    readonly permissions: Permissions;
-}
-
-/**
- * Функция, выполняющая основную логику команды
- */
 interface CommandExecuteable<Args extends unknown[]> {
     (context: CommandContext<Args>): PromiseVoid;
 }
 
-/**
- * Аргумент конструктора команды
- */
+export interface CommandInfo<Args extends unknown[]> {
+    readonly name: string;
+    readonly aliases?: Readonly<NonEmpty<string[]>>;
+    readonly arguments?: ArgumentSequence<Args>;
+    readonly permissions: Permissions;
+}
+
 interface CommandInfoArgument<Args extends unknown[]> {
-    /**
-     * Описание команды
-     */
     readonly description: string;
-
-    /**
-     * Группа, к которой принадлежит команда
-     */
     readonly group: string;
-
-    /**
-     * Как пользоваться командой
-     */
     readonly tutorial: string;
-
-    /**
-     * Права участника сервера (библиотека вызывает [[Permissions.freeze]]!)
-     */
     readonly permissions?: BitFieldResolvable<PermissionString>;
-
-    /**
-     * Функция, выполняющая основную логику команды
-     */
     readonly execute: CommandExecuteable<Args>;
 }
 
 export interface Command<Args extends unknown[]> extends CommandInfo<Args> { }
 
-/**
- * Объект, хранящий информацию команды и её логику
- */
 export class Command<Args extends unknown[]> {
-    /**
-     * Функция, выполняющая основную логику команды
-     */
     readonly executeable: CommandExecuteable<Args>;
 
-    /**
-     * Термины команды для переводчика
-     */
     readonly infoTerms: TermCollection<{
         readonly [I in "description" | "group" | "tutorial" | `argument_${Indexes<Args>}_description`]: []
     }>;
 
-    /**
-     * @param definition информация о команде
-     */
     constructor(definition: Overwrite<CommandInfo<Args>, CommandInfoArgument<Args>>) {
         const { execute: executeable, permissions, name, aliases, arguments: args, ...info } = definition;
 
@@ -123,18 +67,10 @@ export class Command<Args extends unknown[]> {
         Object.freeze(this);
     }
 
-    /**
-     * @returns true, если участник сервера может использовать команду
-     */
     canBeExecutedBy(member: GuildMember): boolean {
         return !member.permissions.missing(this.permissions.bitfield).length;
     }
 
-    /**
-     * Запускает команду
-     * @param bot ссылка на бота
-     * @param message сообщение, которое запускает команду
-     */
     async execute(bot: Bot, message: GuildMessage): Promise<CommandContext<Args>> {
         assert(this.canBeExecutedBy(message.member), message.member.displayName + ` can\`t run command '${this.name}'`);
 
